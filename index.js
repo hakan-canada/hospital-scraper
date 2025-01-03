d8d04132d305dba45602f22af8998b6f54c40fdd
hakan-canada <hakan.canada1@gmail.com>
2025-01-03 12:22:47 -0500

    Update PORT config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 async function scrapeHospitals() {
   const browser = await puppeteer.launch({
